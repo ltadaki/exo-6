68cee0e3aafdbd421893506fa063800b5d9e3985
ltadaki <103495607+ltadaki@users.noreply.github.com>
2022-04-19 11:21:56 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        `)
+})
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
